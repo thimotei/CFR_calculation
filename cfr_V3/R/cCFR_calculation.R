f80e6a924619c2adab4616fbf742fe4ed5c6f367
Tim Russell <timothywilliamrussell@gmail.com>
2020-03-02 00:11:02 +0000

    added all available international data, plots and estimates

--- cfr_V3/R/cCFR_calculation.R
+++ cfr_V3/R/cCFR_calculation.R
@@ -77,48 +77,6 @@ output_cfr_timeseries <- function(main_data_in, delay_fun){
 }
 
 
-  # Calculate final CI and output
-output_estimates <- function(main_data_in, delay_fun)
-{
-  
-  # Load data and omit initial missing entries
-  data_1 <- main_data_in[!is.na(main_data_in$new_cases),] 
-  data_1 <- data_1 %>% mutate(new_cases = scaled_reporting*new_cases,
-                              new_deaths = new_deaths) # scale up based on exported case estimates
-  
-  # Calculate corrected CFR
-  store_cfr <- NULL
-  for(tt in 1:nrow(data_1))
-  {
-    store_cfr <- rbind(store_cfr,scale_cfr(data_1[1:tt,], delay_fun))
-  }
-  
-  # store in tibble
-  store_cfr <- as_tibble(store_cfr); names(store_cfr) <- c("naive","cCFR","deaths","known_outcomes","cases")
-  
-  tt_max <- nrow(data_1)
-  
-  # calculate confidence intervals on estimates
-  CI_nCFR_raw <- c.input.text(bin_conf(data_1[tt_max,]$deaths,data_1[tt_max,]$cases))
-  CI_nCFR <- c.input.text(bin_conf(data_1[tt_max,]$deaths,round(scaled_reporting*data_1[tt_max,]$cases)))
-  CI_cCFR <- c.input.text(bin_conf(store_cfr$deaths[tt_max], round(scaled_reporting*(store_cfr$known_outcomes[tt_max]))))
-  
-  
-  output_estimate_data <- rbind(c("Naive CFR (raw data)",CI_nCFR_raw),
-                                c("Naive CFR (scaled data)",CI_nCFR),
-                                c("Adjusted CFR (scaled data)",CI_cCFR))
-  
-  output_estimate_data <- as_tibble(output_estimate_data)
-  names(output_estimate_data) <- c("Data/method","Estimate")
-  
-  #write_csv(output_estimate_data,paste0("~/cfr_table_scale_",scaled_reporting,"_",tail(data_1,1)$date,".csv"))
-  
-  return(output_estimate_data)
-  
-}
-
-# 
-
 calculate_CIs_nCFR <- function(data_1)
 {
   
@@ -143,20 +101,27 @@ calculate_CIs_nCFR <- function(data_1)
 }
 
 
-calculate_CIs_cCFR <- function(data_1)
+calculate_CIs_cCFR <- function(data_1, delay_dist)
 {
   
   CI_cCFR_raw_mid <- NA
   CI_cCFR_raw_low <- NA
   CI_cCFR_raw_high <- NA
-  
-   for(i in 1:nrow(data_1))
-   {
-     
-     CI_cCFR_raw_mid[i] <- signif(bin_conf(data_1[i,]$deaths, data_1[i,]$known_outcomes)[1]*100, 3)
-     CI_cCFR_raw_low[i] <- signif(bin_conf(data_1[i,]$deaths, data_1[i,]$known_outcomes)[2]*100, 3)
-     CI_cCFR_raw_high[i] <- signif(bin_conf(data_1[i,]$deaths, data_1[i,]$known_outcomes)[3]*100, 3)
-     
+    
+    for(i in 1:nrow(data_1))
+    {
+      if(data_1[i,]$known_outcomes >= data_1[i,]$deaths)
+      {
+        CI_cCFR_raw_mid[i]  <- signif(bin_conf(data_1[i,]$deaths, data_1[i,]$known_outcomes)[1]*100, 3)
+        CI_cCFR_raw_low[i]  <- signif(bin_conf(data_1[i,]$deaths, data_1[i,]$known_outcomes)[2]*100, 3)
+        CI_cCFR_raw_high[i] <- signif(bin_conf(data_1[i,]$deaths, data_1[i,]$known_outcomes)[3]*100, 3)
+      }
+      else if(data_1[i,]$known_outcomes < data_1[i,]$deaths)
+      {
+        CI_cCFR_raw_mid[i]  <- signif(bin_conf(data_1[i,]$known_outcomes, data_1[i,]$deaths)[1]*100, 3)
+        CI_cCFR_raw_low[i]  <- signif(bin_conf(data_1[i,]$known_outcomes, data_1[i,]$deaths)[2]*100, 3)
+        CI_cCFR_raw_high[i] <- signif(bin_conf(data_1[i,]$known_outcomes, data_1[i,]$deaths)[3]*100, 3)
+      }
    }
    
    output_CIs <-  data.frame(date = data_1$date,
@@ -167,6 +132,25 @@ calculate_CIs_cCFR <- function(data_1)
    
 }
 
+nCFR_with_CIs_main_time_series <- function(data_1, delay_dist)
+{
+  
+  dateRange <- seq(as.Date(min(data_1$date)), as.Date(max(data_1$date)), 1)
+  
+  CFRVariablename1 <- as.name(paste0("cCFRRealisticTime_", deparse(substitute(data_1))))
+  CFRVariablename2 <- as.name(paste0("cCFRRealisticTimeConstrained_", deparse(substitute(data_1))))
+  CFRVariablename3 <- as.name(paste0("CFRCIs_", deparse(substitute(data_1))))
+  
+  output_cfr_timeseries(data_1, delay_dist)  -> CFRVariablename1
+  cbind(date = dateRange, CFRVariablename1) -> CFRVariablename1
+  subset(CFRVariablename1, deaths > 0 & known_outcomes > 0) -> CFRVariablename2
+  calculate_CIs_nCFR(CFRVariablename2) -> CFRVariablename3
+  
+  CFRVariablename3$date <- as.Date(CFRVariablename3$date)
+  
+  return(CFRVariablename3)
+  
+}
 
 
 cCFR_with_CIs_main_time_series <- function(data_1, delay_dist)
@@ -180,12 +164,36 @@ cCFR_with_CIs_main_time_series <- function(data_1, delay_dist)
   
   output_cfr_timeseries(data_1, delay_dist)  -> CFRVariablename1
   cbind(date = dateRange, CFRVariablename1) -> CFRVariablename1
-  subset(CFRVariablename1, known_outcomes > deaths & deaths > 0 & known_outcomes > 0) -> CFRVariablename2
+  subset(CFRVariablename1, deaths > 0 & known_outcomes > 0) -> CFRVariablename2
   calculate_CIs_cCFR(CFRVariablename2) -> CFRVariablename3
+  
+  CFRVariablename3$date <- as.Date(CFRVariablename3$date)
   
   return(CFRVariablename3)
   
 }
+
+computenCFRTimeSeries <- function(data_in)
+{
+  
+  data_in_2 <- pad(data_in)
+  data_in_2 <- data_in_2   %>% mutate_if(is.integer, ~replace(., is.na(.), 0)) 
+  data_in_2 <- data_in_2 %>% mutate_if(is.factor, ~replace(., is.na(.), data_in$country[1])) 
+  
+  currentnCFRTimeSeries <- nCFR_with_CIs_main_time_series(data_in_2, hospitalisation_to_death_truncated)
+  
+}
+
+computecCFRTimeSeries <- function(data_in)
+{
+  
+  data_in_2 <- pad(data_in)
+  data_in_2 <- data_in_2   %>% mutate_if(is.integer, ~replace(., is.na(.), 0)) 
+  data_in_2 <- data_in_2 %>% mutate_if(is.factor, ~replace(., is.na(.), data_in$country[1])) 
+  
+  currentcCFRTimeSeries <- cCFR_with_CIs_main_time_series(data_in_2, hospitalisation_to_death_truncated)
+}
+
 
 
 scale_by_age_distribution_cases <- function(data_1, sf_data)
--- cfr_V3/scripts/main_script.R
+++ cfr_V3/scripts/main_script.R
@@ -61,94 +61,17 @@ hospitalisation_to_death_truncated <- function(x)
   dlnorm(x, muHDT, sigmaHDT)
 }
 
-
-
 source("R/cCFR_calculation.R")
+source("R/plotting_functions.R")
 
-cCFRTimeSeriesReal <- output_cfr_timeseries(allTogetherReal, onset_to_death)
-cCFRTimeSeriesReal <- cbind(date=dateRangeRealData, cCFRTimeSeriesReal)
-cCFRTimeSeriesRealConstrained <- subset(cCFRTimeSeriesReal, known_outcomes > deaths & deaths > 0 & known_outcomes > 0)
-cfrPointEstimateReal <- output_estimates(allTogetherReal, onset_to_death)
-
-cCFRTimeSeriesInferred <- output_cfr_timeseries(allTogetherInferred, onset_to_death)
-cCFRTimeSeriesInferred <- cbind(date=dateRangeIncidence, cCFRTimeSeriesInferred)
-cCFRTimeSeriesInferredConstrained <- subset(cCFRTimeSeriesInferred, known_outcomes > deaths & deaths > 0 & known_outcomes > 0)
-cCFRPointEstimateInferred <- output_estimates(allTogetherInferred, onset_to_death)
+ChinaData <- subset(allTogetherInternational, country == "China")
+JapanData <- subset(allTogetherInternational, country == "Japan")
+KoreaData <- subset(allTogetherInternational, country == "South Korea")
+IranData  <- subset(allTogetherInternational, country == "Iran")
 
 
-nCFRRealCIs <- calculate_CIs_nCFR(cCFRTimeSeriesRealConstrained)
-nCFRInferredCIs <- calculate_CIs_nCFR(cCFRTimeSeriesInferredConstrained)
-
-cCFRRealCIs <- calculate_CIs_cCFR(cCFRTimeSeriesRealConstrained)
-cCFRInferredCIs <- calculate_CIs_cCFR(cCFRTimeSeriesInferredConstrained)
-
-# international cases without Diamond princess cruise ship included
-
-cCFRTimeSeriesInternational <- output_cfr_timeseries(internationalDataRaw, confirmation_to_death)
-cCFRTimeSeriesInternational <- cbind(date=internationalDataRaw$date, cCFRTimeSeriesInternational)
-cCFRTimeSeriesInternationalConstrained <- subset(cCFRTimeSeriesInternational, known_outcomes > deaths & deaths > 0 & known_outcomes > 0)
-
-nCFRInternationalCIs <- calculate_CIs_nCFR(cCFRTimeSeriesInternationalConstrained)
-cCFRInternationalCIs <- calculate_CIs_cCFR(cCFRTimeSeriesInternationalConstrained)
-
-# international cases with Diamond princess cruise ship included
-
-# combining international data and cruise ship data
-
-relevantInternationalSubset <- subset(internationalDataRaw[c(1,6,7)], 
-                                      date >= head(cruiseShipDataRaw$date, n = 1) & 
-                                      date <= tail(cruiseShipDataRaw$date, n = 1))
-
-combinedCases <- relevantInternationalSubset$new_cases + cruiseShipDataRaw$new_cases
-combinedDeaths <- relevantInternationalSubset$new_deaths + cruiseShipDataRaw$new_deaths
-
-internationalCruiseData <- data.frame(date = relevantInternationalSubset$date,
-                           new_cases = combinedCases,
-                           new_deaths = combinedDeaths)
-
-
-
-cCFRTimeSeriesInternationalCruise <- output_cfr_timeseries(internationalCruiseData, confirmation_to_death)
-cCFRTimeSeriesInternationalCruise <- cbind(date=internationalCruiseData$date, cCFRTimeSeriesInternationalCruise)
-cCFRTimeSeriesInternationalCruiseConstrained <- subset(cCFRTimeSeriesInternationalCruise,
-                                                       known_outcomes > deaths & deaths > 0 & known_outcomes > 0)
-#cfrPointEstimateInternational <- output_estimates(cCFRTimeSeriesInternationalConstrained)
-
-nCFRInternationalCruiseCIs <- calculate_CIs_nCFR(cCFRTimeSeriesInternationalCruiseConstrained)
-cCFRInternationalCruiseCIs <- calculate_CIs_cCFR(cCFRTimeSeriesInternationalCruiseConstrained)
-
-
-# cruise ship cases only
-
-cCFRTimeSeriesCruise <- output_cfr_timeseries(cruiseShipDataRaw, confirmation_to_death)
-cCFRTimeSeriesCruise <- cbind(date=cruiseShipDataRaw$date, cCFRTimeSeriesCruise)
-cCFRTimeSeriesCruiseConstrained <- subset(cCFRTimeSeriesCruise, known_outcomes > deaths & deaths > 0 & known_outcomes > 0)
-#cfrPointEstimateInternational <- output_estimates(cCFRTimeSeriesInternationalConstrained)
-
-nCFRInternationalJustCruiseCIs <- calculate_CIs_nCFR(cCFRTimeSeriesCruiseConstrained)
-cCFRInternationalustCruiseCIs <- calculate_CIs_cCFR(cCFRTimeSeriesCruiseConstrained)
-
-
-# cruise ship cases only
-
-cCFRTimeSeriesCruise <- output_cfr_timeseries(cruiseShipDataRaw, confirmation_to_death)
-cCFRTimeSeriesCruise <- cbind(date=cruiseShipDataRaw$date, cCFRTimeSeriesCruise)
-cCFRTimeSeriesCruiseConstrained <- subset(cCFRTimeSeriesCruise, known_outcomes > deaths & deaths > 0 & known_outcomes > 0)
-#cfrPointEstimateInternational <- output_estimates(cCFRTimeSeriesInternationalConstrained)
-
-nCFRInternationalJustCruiseCIs <- calculate_CIs_nCFR(cCFRTimeSeriesCruiseConstrained)
-cCFRInternationalustCruiseCIs <- calculate_CIs_cCFR(cCFRTimeSeriesCruiseConstrained)
-
-# running Christian's data and distribution as a check
-
-christiansData <- read.csv("data/ncov_cases_20200217.csv")
-
-names(christiansData)[names(christiansData) == "cases"] <- "new_cases"
-names(christiansData)[names(christiansData) == "deaths"] <- "new_deaths"
-
-cCFRTimeSeriesChristian <- output_cfr_timeseries(christiansData, linton_dist)
-cCFRTimeSeriesChristian <- cbind(date=christiansData$date, cCFRTimeSeriesChristian)
-cCFRTimeSeriesChristianConstrained <- subset(cCFRTimeSeriesChristian, known_outcomes > deaths & deaths > 0 & known_outcomes > 0)
-
-nCFRChristianCIs <- calculate_CIs_nCFR(cCFRTimeSeriesChristianConstrained)
-cCFRChristianCIs <- calculate_CIs_cCFR(cCFRTimeSeriesChristianConstrained)
+master_plot(allTogetherInferred, 1, 48, "topleft")
+master_plot(ChinaData, 15, 15, "bottomright")
+master_plot(JapanData, 1, 1, "top")
+master_plot(KoreaData, 1, 1, "topright")
+master_plot(IranData, 1, 1, "right")
