2b461642a98b491572ea80ab10cae5af4a44c2e0
adamkucharski <adam.kucharski@lshtm.ac.uk>
2020-04-02 21:38:40 +0100

    Add splines

--- global_estimates/scripts/main_script_temporal.R
+++ global_estimates/scripts/main_script_temporal.R
@@ -8,6 +8,7 @@
 # Load libraries
 library(tidyverse)
 library(padr)
+library(mgcv)
 
 # Set paths
 setwd("~/Documents/lshtm/github repos/CFR_calculation/global_estimates/")
@@ -87,34 +88,57 @@ allTogetherCleanA <- allDatDesc %>%
 # Plot rough reporting over time -----------------------------------------
 
 #countrypick <- "Denmark"
-countrypickList <- c("Denmark","United_Kingdom","Germany","China","South_Korea","United_States_of_America")
+countrypickList <- c("China","United_Kingdom","Germany","South_Korea",
+                     "United_States_of_America","Denmark","Spain","Portugal")
 
-par(mfcol=c(2,6),mar=c(3,3,1,1),mgp=c(2,0.6,0))
+par(mfrow=c(2,4),mar=c(3,3,1,1),mgp=c(2,0.6,0))
 
-for(ii in 1:6){
+for(ii in 1:length(countrypickList)){
   
   countrypick <- countrypickList[ii]
   
   data_1_in_Pick <- allTogetherCleanA %>% filter(country==countrypick)
   true_cfr <- 1.4/100
+  cum_sum_d <- cumsum(data_1_in_Pick$new_deaths)
   
   out_cfr <- scale_cfr_temporal(data_1_in_Pick)
   reporting_estimate <- (true_cfr/out_cfr$cCFR) %>% pmin(.,1)
+  reporting_estimate[cum_sum_d<15] <- NA # remove before deaths
+  xx_date <- data_1_in_Pick$date-zmeanHDT
   
-  # Rough plot
-  plot(data_1_in_Pick$date-zmeanHDT,reporting_estimate,ylim=c(0,1),ylab="reported",xlab="",main=countrypick)
+  # Rough plot & set colours
+  plot(xx_date,reporting_estimate,ylim=c(0,1),ylab="reported",xlab="",main=countrypick)
+  
+  col_def <-   list(col1=rgb(1,0,0),col2=rgb(0.8,0.6,0),col3=rgb(0,0.8,0.8),col4=rgb(0.1,0.4,0.1),col5=rgb(1,0.4,1),col6=rgb(0.2,0,0.8),"dark grey")
+  col_def_F <- list(col1=rgb(1,0,0,0.2),col2=rgb(0.8,0.6,0,0.5),col3=rgb(0,0.8,0.8,0.5),col5=rgb(0.1,0.4,0.1,0.5),col6=rgb(1,0.4,1,0.5),col7=rgb(0.2,0,0.8,0.5),"grey")
+  
+  # Collate data and fit GAM model
+  data_fit <- cbind(xx_date,reporting_estimate)
+  data_fit <- as_tibble(data_fit); names(data_fit) <- c("date","report")
+  
+  modelB.P <- gam(report ~ s(date) , data = data_fit,family = "gaussian")
+  
+  # Predict data
+  xx_pred <- xx_date[cum_sum_d>=15]
+  preds <- predict(modelB.P, newdata = list(date=as.numeric(xx_pred)), type = "link", se.fit = TRUE)
+  critval <- 1.96; upperCI <- preds$fit + (critval * preds$se.fit); lowerCI <- preds$fit - (critval * preds$se.fit)
+  fit <- preds$fit
+  fitPlotF <- modelB.P$family$linkinv(fit); CI1plotF <- modelB.P$family$linkinv(upperCI);  CI2plotF <- modelB.P$family$linkinv(lowerCI)
+  
+  polygon(c(xx_pred,rev(xx_pred)),c(CI1plotF,rev(CI2plotF)),col=col_def_F[[1]],lty=0)
+  lines(xx_pred, fitPlotF ,col=col_def[[1]],lwd=2)
   
   # Estimate true symptomatic cases
   
   # Align case data with reporting
-  estimate_cases <- head(data_1_in_Pick$new_cases,-zmeanHDT)/tail(reporting_estimate,-zmeanHDT) 
-  date_match <- head(data_1_in_Pick$date,-zmeanHDT)
-  
-  plot(date_match,estimate_cases,ylab="cases",xlab="",log="y",main=countrypick)
-  lines(data_1_in_Pick$date,data_1_in_Pick$new_cases,col="blue")
+  # estimate_cases <- head(data_1_in_Pick$new_cases,-zmeanHDT)/tail(reporting_estimate,-zmeanHDT) 
+  # date_match <- head(data_1_in_Pick$date,-zmeanHDT)
+  # 
+  #plot(date_match,estimate_cases,ylab="cases",xlab="",log="y",main=countrypick)
+  #lines(data_1_in_Pick$date,data_1_in_Pick$new_cases,col="blue")
 }
   
-dev.copy(png,paste("outputs/calc_1.png",sep=""),units="cm",width=25,height=10,res=150)
+dev.copy(png,paste("outputs/calc_1.png",sep=""),units="cm",width=20,height=12,res=150)
 dev.off()
   
   
